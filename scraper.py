c75a3e215559dd09f7bc7fafd066ed48081dc8ee
chiffonkla <116045907+chiffonkla@users.noreply.github.com>
2026-04-28 10:24:17 -0700

    Added patterns for filtering bad URLs and extensions

--- scraper.py
+++ scraper.py
@@ -10,7 +10,41 @@ ALLOWED_DOMAINS = (
     "stat.uci.edu",
 )
 
+MIN_WORDS_PER_PAGE = 50
 
+BAD_EXTENSIONS = re.compile(
+    r".*\.("
+    r"css|js|json|xml|rss|atom"
+    r"|bmp|gif|jpe?g|ico|png|tiff?|svg|webp"
+    r"|mp[234]|mid|ram|wav|m4[av]|wma|ogg|flac|aac"
+    r"|avi|mov|mpeg|mkv|ogv|webm|wmv|swf|rm|smil"
+    r"|pdf|ps|eps|tex|ppt|pptx|ppsx|thmx|mso|rtf|key|odp|ods|odt"
+    r"|doc|docx|xls|xlsx|csv|tsv|dat|data|names|arff"
+    r"|exe|msi|bin|apk|dll|jar|war|class"
+    r"|bz2|tar|gz|tgz|7z|rar|zip|lz|lzma|xz"
+    r"|psd|ai|dmg|iso|epub|cnf|sha1|sha256|md5|asc|sig"
+    r"|woff2?|ttf|otf|eot|sql|sqlite|db|ipynb|mat|fig|nb"
+    r")$",
+    re.IGNORECASE,
+)
+
+BAD_PATHS = re.compile(
+    r"(?:/wp-(?:login|admin|json)|/login|/signup|/signin|/logout|/register"
+    r"|/share|/sharer|/replytocom|/feed|/atom|/rss"
+    r"|/trackback|/pingback|/embed|/attachment|/cas/login)",
+    re.IGNORECASE,
+)
+
+BAD_QUERY_PARAMS = frozenset({
+    "ical", "outlook-ical", "share", "sharer",
+    "action", "do",
+    "rev", "revision", "diff", "version", "oldid", "history",
+    "redirect_to", "redirect", "replytocom",
+    "format", "print", "printable",
+    "phpsessid", "sid", "session",
+    "filter", "sort_by", "orderby",
+    "tribe-bar-date", "eventdate",
+})
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
