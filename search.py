57e137e2c9007b2e0cefd240113d9ced987de2a1
chiffonkla <116045907+chiffonkla@users.noreply.github.com>
2026-05-20 22:37:13 -0700

    Add initial search.py

---
+++ search.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+from corpus_io import get_url, read_urls
+from index_reader import line_for_term, read_lexicon
+from parse_document import stem_query
+from postings import parse_line
+from query import intersect
+
+
+def run_query(lex, index_path, urls, text):
+
+
+
+def main():
+
+
